76c8229dcc55d333666f87a4e6c63449b64cce88
Ryan <rmurph1800@gmail.com>
2021-10-09 19:18:49 -0400

    added db query

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+const mysql = require("mysql2");
+
 const express = require("express");
 
 const PORT = process.env.PORT || 3001;
@@ -6,11 +8,25 @@ const app = express();
 app.use(express.urlencoded({ extended: false}));
 app.use(express.json());
 
+const db = mysql.createConnection(
+    {
+        host: "localhost",
+        user: "root",
+        password: "Dundermifflin1!",
+        database: "election"
+    },
+    console.log("Connected to the elecion database.")
+);
+
 // app.get("/", (req, res) => {
 //     res.json({
 //         message: "Hello World"
 //     });
 // });
+
+db.query(`SELECT * FROM candidates` , (err, rows) => {
+    console.log(rows);
+});
 
 app.use((req, res) => {
     res.status(404).end();
